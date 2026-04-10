80d37b19702e9e4f9df6d36c9f5058e3c3753695
Oscar Notebook <oskharm12@gmail.com>
2026-04-09 22:16:56 -0300

    teste

--- src/lib/tenant.ts
+++ src/lib/tenant.ts
@@ -6,22 +6,27 @@ export async function resolveTenant() {
 
   let host = headersList.get("host") || ""
 
-  // remove porta se existir (ex: :3000)
-  //host = host.split(":")[0]
-host = 'https://dev-teste.nolevel.hiskra.com.br/login'
+  // =========================
+  // SIMULAÇÃO (APENAS DEV)
+  // =========================
+  // Em produção o host NÃO vem com protocolo nem path
+  // Exemplo real: dev-teste.nolevel.hiskra.com.br
+  if (process.env.NODE_ENV === "development") {
+    host = "dev-teste.nolevel.hiskra.com.br"
+  }
+
+  // remove porta (ex: :3000)
+  host = host.split(":")[0]
+
   let slug: string | null = null
 
   if (host.includes("localhost")) {
     slug = "dev-teste"
   } else {
     const parts = host.split(".")
-    console.log('Host parts:', parts[0])
 
     if (parts.length >= 3) {
-      // pega sempre o primeiro nível como tenant
-      slug = parts[0] // aqui vai vir o http tambem, então precisa remover o http:// ou https://
-      slug = slug.replace("http://", "").replace("https://", "")
-      console.log('Slug:', slug)
+      slug = parts[0]
     }
   }
 
@@ -30,15 +35,13 @@ host = 'https://dev-teste.nolevel.hiskra.com.br/login'
   }
 
   const empresa = await empresaRepository.findAll().then((empresas) =>
-    empresas.find((e) => e.nome === slug)
+    empresas.find((e) => e.nome === slug) // ideal: trocar para e.slug no futuro
   )
 
   if (!empresa) {
     throw new Error(`Empresa não encontrada para o tenant: ${slug}`)
   }
-
-  console.log(empresa.databaseUrl)
-
+  console.log("bd url",empresa.databaseUrl)
   return {
     slug,
     empresa,
