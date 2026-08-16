226c419eedbc6c721890173928ebf4a66d24e6ee
Oscar Notebook <oskharm12@gmail.com>
2026-08-16 00:58:53 -0300

    asdfqewr

--- next.config.ts
+++ next.config.ts
@@ -21,9 +21,8 @@ const nextConfig = {
         pathname: "/storage/v1/object/public/**",
       },
       {
-        protocol: "http",
-        hostname: "177.153.33.179",
-        port: "8000",
+        protocol: "https",
+        hostname: "wpiecnptksrnaipbfiwi.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
       {
@@ -32,11 +31,7 @@ const nextConfig = {
         port: "8000",
         pathname: "/storage/v1/object/public/**",
       },
-/*       {
-        protocol: "https",
-        hostname: "tcgvuhoyojgdnzobmxxl.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      }, */
+
       {
         protocol: "https",
         hostname: "itgagizimkgskiualdzx.supabase.co",
